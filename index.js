1a2b4d58d4dbc7b708a20e72f71aa6140b83ed79
Babisha Manandhar <babisha.manandhar@gmail.com>
2018-02-12 17:44:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function theBeatlesPlay(musicians, instruments) {
   const array = []
   
@@ -16,10 +17,32 @@ function johnLennonFacts(facts) {
     i++
   }
   return array
+=======
+function theBeatlesPlay(musicians,instruments) {
+  const array= []
+  
+  for (let i= 0; l = musicians.length; i < l; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+    
+  return array
+}
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return shoutedFacts
+>>>>>>> df377f8df457a334c5d87178af469e2ddfe491c5
 }
 
 function iLoveTheBeatles(n) {
   const arr = []
+<<<<<<< HEAD
   
   do {
     arr.push('I love the Beatles!')
@@ -27,5 +50,13 @@ function iLoveTheBeatles(n) {
   }
   while (n < 15)
   
+=======
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+>>>>>>> df377f8df457a334c5d87178af469e2ddfe491c5
   return arr
 }
